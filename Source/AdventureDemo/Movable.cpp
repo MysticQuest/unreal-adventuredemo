3dc19abc1d2b083c0a5c20bdff0e8f271374035d
Mystic Quest <l1skou@gmail.com>
2023-02-08 02:52:46 +0200

    adds movable class

---
+++ Source/AdventureDemo/Movable.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Movable.h"
+
+
+// Sets default values for this component's properties
+UMovable::UMovable()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMovable::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialLocation = GetOwner()->GetActorLocation();
+	
+}
+
+
+// Called every frame
+void UMovable::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (ShouldMove)
+	{
+		FVector CurrentLocation = GetOwner()->GetActorLocation();
+		FVector TargetLocation = InitialLocation + MoveOffset;
+		float Speed = FVector::Distance(InitialLocation, TargetLocation) / MoveDuration;
+
+		FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+		GetOwner()->SetActorLocation(NewLocation);
+	}
+}
+
